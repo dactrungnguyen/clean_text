3536749c41925f3679d32f29ac6b0b12317b330d
Dac-Trung Nguyen <dactrung.nguyen@gmail.com>
2020-05-23 11:14:55 +0200

    First draft of infra

--- back/__main__.py
+++ back/__main__.py
@@ -1,5 +1,11 @@
 from sys import stdin
+from time import sleep
 from back.worker import clean_text
 
-print('Cleaned text:')
-print(clean_text(stdin.read()))
+while True:
+    try:
+        print('I am running')
+        sleep(1)
+    except KeyboardInterrupt:
+        print('\nBye!')
+        exit(1)
