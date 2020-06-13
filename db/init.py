f868a32ecfe1c1a9ba69c8bb5067122099c3811b
Dac-Trung Nguyen <dactrung.nguyen@gmail.com>
2020-06-13 12:26:18 +0200

    Add API endpoints

--- db/init.py
+++ db/init.py
@@ -2,5 +2,5 @@ import sqlite3
 
 c = sqlite3.connect('db/clean_text.db')
 c.execute('DROP TABLE IF EXISTS runs')
-c.execute('CREATE TABLE runs (id TEXT, input TEXT, output TEXT)')
+c.execute('CREATE TABLE runs (id TEXT, state TEXT, input TEXT, output TEXT)')
 c.close()
--- src/api/api.py
+++ src/api/api.py
@@ -22,15 +22,37 @@ class HelloResource(Resource):
 text_post_parser = reqparse.RequestParser()
 text_post_parser.add_argument('file', type=FileStorage, location='files')
 
-run_post_response_model = api.model('run_post_response', {
+run_model = api.model('run', {
     'id': fields.String,
-    'status': fields.String,
+    'input': fields.String,
+    'output': fields.String,
+    'state': fields.String,
+})
+run_list_model = api.model('run_list', {
+    'runs': fields.List(fields.Nested(run_model)),
 })
 
 @api.route('/runs')
-@api.expect(text_post_parser)
-class RunsResource(Resource):
-    @api.marshal_with(run_post_response_model)
+class AllRuns(Resource):
+    @api.marshal_with(run_list_model)
+    def get(self):
+        with sqlite3.connect(DB_PATH) as connection:
+            cursor = connection.cursor()
+            cursor.execute((
+                'SELECT r.id, r.state, r.input, r.output FROM runs AS r '
+            ))
+            runs = cursor.fetchall()
+        return {'runs': [
+            {
+                'id': run[0],
+                'state': run[1],
+                'input': run[2],
+                'output': run[3],
+            } for run in runs
+        ]}
+
+    @api.expect(text_post_parser)
+    @api.marshal_with(run_model)
     def post(self):
         args = text_post_parser.parse_args()
         uploaded = args['file']
@@ -39,8 +61,29 @@ class RunsResource(Resource):
         with sqlite3.connect(DB_PATH) as connection:
             cursor = connection.cursor()
             cursor.execute((
-                'INSERT INTO runs (id, input, output) '
-                'VALUES (?, ?, ?)'
-            ), (run_id, input_text, None))
+                'INSERT INTO runs (id, state, input, output) '
+                'VALUES (?, ?, ?, ?)'
+            ), (run_id, 'SUBMITTED', input_text, None))
 
-        return {'id': run_id, 'status': 'SUBMITTED'}
+        return {'id': run_id, 'state': 'SUBMITTED', 'input': input_text, 'output': None}
+
+
+@api.route('/runs/<run_id>')
+class RunResource(Resource):
+    @api.marshal_with(run_model)
+    def get(self, run_id: str):
+        with sqlite3.connect(DB_PATH) as connection:
+            cursor = connection.cursor()
+            cursor.execute((
+                'SELECT r.id, r.state, r.input, r.output FROM runs AS r '
+                f'WHERE r.id = "{run_id}"'
+            ))
+            runs = cursor.fetchall()
+        assert len(runs) == 1
+        run = runs[0]
+        return {
+            'id': run[0],
+            'state': run[1],
+            'input': run[2],
+            'output': run[3],
+        }
